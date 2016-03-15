7c515fd8ab0ba358f10cf7e55376e4576e77f3d6
Jonathan Kellermann <jonathankellermann75@gmail.com>
2016-03-15 18:30:54 +0200

    Project Intial

---
+++ TicTacToe/TicTacToe.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class TicTacToe
+{
+private: 
+	int player;//which players turn
+	int move;// what move we on max 9
+	char grid[3][3];
+public:
+
+
+
+};
